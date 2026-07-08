31097023f1da86ee803ae9772d1bb0a5765484eb
Khaemon Edwards <khaemonedwards@yahoo.com>
2026-07-08 16:12:22 -0400

    linting views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -104,7 +104,9 @@ def get_cars(request):
     return JsonResponse({"CarModels": cars})
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
-def get_dealerships(request, state = "All"):
+
+
+def get_dealerships(request, state="All"):
     if (state == "All"):
         endpoint = 'fetchDealers'
     else:
@@ -114,8 +116,10 @@ def get_dealerships(request, state = "All"):
 # ...
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
+
+
 def get_dealer_reviews(request, dealer_id):
-    if(dealer_id):
+    if (dealer_id):
         endpoint = 'fetchReviews/dealer/' + str(dealer_id)
         reviews = get_request(endpoint)
         for review_detail in reviews:
@@ -128,6 +132,8 @@ def get_dealer_reviews(request, dealer_id):
 # ...
 
 # Create a `get_dealer_details` view to render the dealer details
+
+
 def get_dealer_details(request, dealer_id):
     if (dealer_id):
         endpoint = 'fetchDealer' + str(dealer_id)
@@ -138,8 +144,10 @@ def get_dealer_details(request, dealer_id):
 # ...
 
 # Create a `add_review` view to submit a review
+
+
 def add_review(request):
-    if (request.user.is_anonymous == False):
+    if (request.user.is_anonymous is False):
         data = json.loads(request.body)
         try:
             response = post_review(data)
