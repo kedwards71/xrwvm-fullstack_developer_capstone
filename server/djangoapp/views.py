8ffe8c9c64c2e367b36a3e19e1758aa8b0d67c12
Khaemon Edwards <khaemonedwards@yahoo.com>
2026-07-08 16:03:13 -0400

    linting views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -48,6 +48,7 @@ def logout_request(request):
 
 # Create a `registration` view to handle sign up request
 
+
 @csrf_exempt
 def registration(request):
     # Load JSON data from the request body
@@ -83,13 +84,16 @@ def registration(request):
         data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
 # ...
+
+
 # get_cars view to render the list of cars
 def get_cars(request):
     count = CarMake.objects.filter().count()
     print(count)
-    if(count == 0):
+    if (count == 0):
         initiate()
-    car_models = CarModel.objects.select_related('car_make')
+    car_objs = CarModel.objects
+    car_models = car_objs.select_related('car_make')
     cars = []
     for car_model in car_models:
         cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
