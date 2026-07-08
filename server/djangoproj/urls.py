16484c0fe9ba42a1d9b0c3f88b2a7edbc95fe348
Khaemon Edwards <khaemonedwards@yahoo.com>
2026-07-08 15:25:32 -0400

    settings.py lint

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -60,5 +60,4 @@ urlpatterns = [
         'postreview/<int:dealer_id>',
         TemplateView.as_view(template_name="index.html")
         ),
-    
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
