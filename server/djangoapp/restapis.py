28f7c24cd12f82804dd601a9aec5625ab5afae28
Khaemon Edwards <khaemonedwards@yahoo.com>
2026-07-08 21:30:52 -0400

    Backend for inventory

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -11,9 +11,28 @@ backend_url = os.getenv(
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
-
+searchcars_url = os.getenv(
+    'searchcars_url',
+    default="http://localhost:3050/")
 # Add code for get requests to back end
 
+def searchcars_request(endpoint, **kwargs):
+    params=""
+    if (kwargs):
+        for key, value in kwargs.items():
+            params = params+key+"="+value+"&"
+    request_url = searchcars_url+endpoint+"?"+params
+    print("GET from {} ".format(request_url))
+
+    try:
+        # Call get method of quests library with URL and parameters
+        response = requests.get(request_url)
+        return response.json()
+    except:
+        # If any error occurs
+        print("Network exception occurred")
+    finally:
+        print("GET request call complete!")
 
 def get_request(endpoint, **kwargs):
     params = ""
@@ -56,3 +75,4 @@ def post_review(data_dict):
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 # Add code for posting review
+
