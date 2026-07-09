f619a46912a11c2690ede9de8251f0e0305e5e2e
Khaemon Edwards <khaemonedwards@yahoo.com>
2026-07-08 20:07:40 -0400

    Search states with search bar

--- server/frontend/src/components/Dealers/Dealers.jsx
+++ server/frontend/src/components/Dealers/Dealers.jsx
@@ -7,12 +7,32 @@ import review_icon from "../assets/reviewicon.png"
 const Dealers = () => {
   const [dealersList, setDealersList] = useState([]);
   // let [state, setState] = useState("")
-  let [states, setStates] = useState([])
+  let [states, setStates] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [originalDealers, setOriginalDealers] = useState([]);
+
+
 
   // let root_url = window.location.origin
   let dealer_url ="/djangoapp/get_dealers";
   
   let dealer_url_by_state = "/djangoapp/get_dealers/";
+
+  const handleInputChange = (event) =>{
+    const query = event.target.value;
+    setSearchQuery(query);
+    const filtered = originalDealers.filter(dealer => 
+        dealer.state.toLowerCase().includes(query.toLowerCase())
+    );
+    setDealersList(filtered);
+
+  }
+
+  const handleLostFocus = () => {
+    if(!searchQuery) {
+        setDealersList(originalDealers);
+    }
+  }
  
   const filterDealers = async (state) => {
     dealer_url_by_state = dealer_url_by_state+state;
@@ -40,6 +60,7 @@ const Dealers = () => {
 
       setStates(Array.from(new Set(states)))
       setDealersList(all_dealers)
+      setOriginalDealers(all_dealers)
     }
   }
   useEffect(() => {
@@ -60,13 +81,7 @@ return(
       <th>Address</th>
       <th>Zip</th>
       <th>
-      <select name="state" id="state" onChange={(e) => filterDealers(e.target.value)}>
-      <option value="" selected disabled hidden>State</option>
-      <option value="All">All States</option>
-      {states.map(state => (
-          <option value={state}>{state}</option>
-      ))}
-      </select>        
+      <input type="text" placeholder="Search states..." onChange={handleInputChange} onBlur={handleLostFocus} value={searchQuery} />
 
       </th>
       {isLoggedIn ? (
